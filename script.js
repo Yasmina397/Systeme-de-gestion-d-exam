50fcfda2e9efd3a07376fa6b752dd5705813acc0
Yasmina397 <yasminaboumehdi51@gmail.com>
2025-04-19 16:36:33 +0100

    Créer
    Créer un examen

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ document.getElementById('form-examen').addEventListener('submit', function (e) {
         questions: []
     };
 
+
     //TODO : Sauvegarder l'examen dans le localStorage sous une clé basée sur le nom du propriétaire
     const examsKey = 'examens_' + examen.proprietaire;
     const exams = JSON.parse(localStorage.getItem(examsKey)) || [];
@@ -20,5 +21,25 @@ document.getElementById('form-examen').addEventListener('submit', function (e) {
     localStorage.setItem(examsKey, JSON.stringify(exams));
 
     alert('Examen ajouté avec succès !');
+    window.location.href = 'ajouter-question.html';
     this.reset();
 });
+
+
+
+
+
+const proprietaire = getElementById('proprietaire'); // ou récupéré via input
+const examsKey = 'examens_' + proprietaire.toLowerCase().replace(/\s+/g, '_');
+const examens = JSON.parse(localStorage.getItem(examsKey)) || [];
+
+const div = document.getElementById('conteneur');
+const liste = document.createElement('ul');
+
+examens.forEach(exam => {
+    const item = document.createElement('li');
+    item.innerHTML = `${exam.nom} - ${exam.duree} min - ${exam.description}`;
+    liste.appendChild(item);
+});
+
+div.appendChild(liste);
